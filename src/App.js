34ee7aee06ec8b026d21c516de3ee2fde77b6e5c
= <=>
2017-06-16 13:01:24 -0400

    Update to acquire

--- src/App.js
+++ src/App.js
@@ -1,87 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function CardModel(rank,suit)
-{
-    this.Rank = rank;
-    this.Suit = suit;
-
-}
-
-function GetCards(){
-    return[
-        new CardModel("Ace","Clubs"),
-        new CardModel("King","Clubs"),
-        new CardModel("Queen","Clubs"),
-        new CardModel("Jack","Clubs"),
-        new CardModel("Ten","Clubs"),
-        new CardModel("9","Clubs"),
-        new CardModel("8","Clubs"),
-    ];
-}
-
-class Card extends Component{
+class Tile extends Component{
     constructor(props){
         super(props);
-        this.state = {FaceUp:props.FaceUp, Rank:props.Rank, Suit:props.Suit};
+        this.state = {
+            Index: props.Index,
+            Title:props.Title,
+            Selected:false,
+            OnClick: props.OnClick
+        };
+    }
+
+    handleClick = () => {
+        this.setState({Selected:true});
     }
 
     render(){
-        return(
-            <div className={['card', this.state.FaceUp? 'card-face':'card-back'].join(" ")}>
-                {this.state.FaceUp &&
-                    [<div key="rank" className = {['rank', this.state.Rank].join(" ")}>{this.state.Rank}</div>,
-                    <div key="suit" className={['suit', this.state.Suit].join(" ")}>{this.state.Suit}</div>]
-                }
-            </div>
-        );
-    }
-}
-
-class Hand extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {CardModels:props.CardModels}
-    }
-
-    render(){
-
+        let classNames = [
+            'tile',
+            this.state.Selected?'selected':''
+        ].join(" ");
         return (
-        <div className = "hand">
-            {
-                this.state.CardModels &&
-                this.state.CardModels.map((c,index)=>{
-                  return <Card key={index} FaceUp Rank={c.Rank} Suit={c.Suit} />
-                })
+            <div className={classNames} onClick={this.handleClick}>
+                <span>
+                    {this.state.Title}
+                </span>
+            </div>)
+    }
+}
+
+class Board extends Component{
+    constructor(props){
+        super(props);
+        this.state= {tiles:[]}
+        let title = "";
+        let key  = 0;
+        for(let c = 65; c<= 74; c++)
+            for(let i = 1; i<=12;i++) {
+                title = `${i}${String.fromCharCode(c)}`;
+                this.state.tiles.push(<Tile key={key} Index={key} Title={title}/>);
+                key++;
             }
-        </div>
+    }
+
+    placeTile(tile){
+
+    }
+
+    render(){
+        return (
+            <div className="Board">
+                <div className="playField">
+                    {this.state.tiles}
+                </div>
+            </div>
         )
     }
 }
 
 class App extends Component{
-    constructor(props){
-        super(props);
-        this.state = {Cards: GetCards()};
-    }
-
-    handleClick = () => {
-        let c = this.state.Cards;
-        c.push(new CardModel("Ace","Diamonds"));
-        this.setState({Cards:c});
-    };
-
 
 
     render(){
-         return(
-             <div>
-                <Hand CardModels={this.state.Cards}/>
-                <button onClick={this.handleClick}>Add</button>
-             </div>
-
-         );
+        return <Board/>
     }
 }
 
